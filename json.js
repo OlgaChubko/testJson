89a7030561c1ac8824bdfa83cb97b6a98b169e0f
olga <olga.chubko.92@mail.ru>
2017-06-24 23:23:06 +0300

    commit with array

--- json.js
+++ json.js
@@ -1,9 +1,11 @@
 function loadRates() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
-    //http://api.wunderground.com/api/2a08024319044ace/conditions/q/CA/Kiev.json
-    //https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5
-//console.log(xhr);
+
+    var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+
+    var xhr = new XHR();
+    //xhr.open('GET', 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5');
+   // http://api.wunderground.com/api/2a08024319044ace/conditions/q/CA/Kiev.json
+    xhr.open('GET', 'http://192.168.1.5:8080/api/allFilms/', true);
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState != 4) return;
@@ -15,25 +17,28 @@ function loadRates() {
         } else {
             try {
                 var rates = JSON.parse(xhr.responseText);
+                // for privat bank
                 console.log(rates);
-                var rettes = JSON.stringify(rates);
-                console.log(rettes);
-                alert(rettes[1].ccy);
+                //console.log(typeof rates);
+                //console.log("This is object 1:"+ rates[(Object.keys(rates)[1])].station_id);
+                //console.log("This is object 1:"+ rates[(Object.keys(rates)[1])].local_tz_long)
+                //console.log("This is object 1:"+ rates[(Object.keys(rates)[1])].relative_humidity)
+                //console.log("This is object 1:"+ rates[(Object.keys(rates)[1])].feelslike_string)
+
 
             } catch (e) {
                 alert("Некорректный ответ " + e.message);
             }
-            showExchangeRates(rates);
+            //showExchangeRates(rates);
         }
     };
     xhr.send(null);
-
     button.innerHTML = 'Загружаю...';
 }
-function showExchangeRates(rates) {
-    rates.forEach(function (rate) {
-        var li = list.appendChild(document.createElement('li'));
-        li.innerHTML = rate.buy;
-    });
-
-}
+//function showExchangeRates(rates) {
+//    rates.forEach(function (rate) {
+//        var li = list.appendChild(document.createElement('li'));
+//        li.innerHTML = rate.buy;
+//    });
+//
+//}
